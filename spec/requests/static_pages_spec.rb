ccd136cfc2d7d19ec7d1e0c58a8f96cf24cff35e
linjama <matti.linjama@tut.fi>
2013-10-30 20:19:03 +0200

    Added users, finished layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,25 +6,25 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
   
   describe "Help page" do
     before { visit help_path }
     it { should have_content('Help') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
+    it { should have_title(full_title('Help')) }
   end
    
   describe "About page" do
     before { visit about_path }
     it { should have_content('About Us') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | About Us") }
+    it { should have_title(full_title('About Us')) }
   end
   
   describe "Contact page" do
     before {visit contact_path }
     it { should have_content('Contact') }
-    it { should have_title("Ruby on Rails Tutorial Sample App | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
