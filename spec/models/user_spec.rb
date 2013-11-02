36fa631c7fdd2e8db26321d84f7c490f81021ced
linjama <matti.linjama@tut.fi>
2013-11-02 09:30:36 +0200

    User validation completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,12 +9,12 @@ describe User do
   it { should respond_to(:email) }
   it { should be_valid }
   
-  describe "When name is not present"  do
+  describe "when name is not present"  do
     before { @user.name = " " }
     it { should_not be_valid }
   end
   
-  describe "When email is not present"  do
+  describe "when email is not present"  do
     before { @user.email = " " }
     it { should_not be_valid }
   end
@@ -24,12 +24,35 @@ describe User do
     it { should_not be_valid }
   end
   
-  describe "When email format is invalid" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
-    addresses.each do |invalid_address|
-      @user.email = invalid_address
-      expect(@user).not_to be_valid
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = ["user@foo,com", "user_at_foo.org", "example.user@foo.",
+                    "foo@bar_baz.com" "foo@bar+baz.com"]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
     end
   end
   
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = ["user@foo.COM", "A_US-ER@f.b.org", "frst.lst@foo.jp", 
+                   "a-b@baz.cn"]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+  
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+    
+    it { should_not be_valid }
+  end
 end
