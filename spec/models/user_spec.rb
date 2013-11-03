5f91345e7035fb70f069780f60eb77959787dd78
linjama <matti.linjama@tut.fi>
2013-11-03 21:06:19 +0200

    Added secure token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
   
   before do
-    @user = User.new(name: "Example User", email: "user@esample.com",
+    @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   end
   
@@ -12,6 +12,7 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
   
@@ -93,5 +94,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       it { expect(user_for_invalid_password).to be_false }
     end
+  end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
