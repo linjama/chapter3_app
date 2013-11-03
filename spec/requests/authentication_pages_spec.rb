dba139074b65b0deb82e0de8f09ef205da364b2f
linjama <matti.linjama@tut.fi>
2013-11-03 20:50:03 +0200

    Completed invalid login

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,4 +10,34 @@ describe "Authentication" do
     it { should have_content('Login') }
     it { should have_title('Login') }
   end
+  
+  describe "login" do
+    before { visit login_path }
+
+    describe "with invalid information" do
+      before { click_button "Login" }
+      
+      it { should have_title('Login') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      
+      describe "aster visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+    
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Login"
+      end
+      
+      it { should have_title(user.name) }
+      it { should have_link('Profile',    href: user_path(user)) }
+      it { should have_link('Logout',     href: logout_path) }
+      it { should_not have_link('Login',  href: login_path) }    
+    end
+  end
 end
